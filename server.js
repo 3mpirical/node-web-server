ae9102b732da45f7b9fcb34902fae154ca93dd52
3mpirical <3mpirical10510@gmail.com>
2018-07-31 02:05:00 -0600

    created basic configuration, middleware, helpers, and routes

--- server.js
+++ server.js
@@ -1,22 +1,56 @@
 const express = require("express"),
       app     = express(),
-      colors  = require("colors");
+      colors  = require("colors"),
+      hbs     = require("hbs"),
+      fs      = require("fs");
 
 
 // CONFIGURATION ===============
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
 
+// MIDDLEWARE ===============
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `The date is now: ${now} | Method: ${req.method} | URL : ${req.url}`;
+
+  fs.appendFile("server.log", log + "\n", (err) => {
+    if (err) {
+      console.log("Could not write to server log".red);
+      console.log(`${err}`.red);
+    }
+  });
+
+  next();
+});
 
 
+// HELPERS ===============
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", (text) => {
+  return text.toUpperCase();
+});
 // ROUTES ===============
 
 app.get("/", (req, res) => {
-  res.json(colors);
+  res.render("home", {
+    pageTitle: "Home Page",
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: "Welcome To Our Incredibly Boring Site!!"
+  });
 });
 
 
 app.get("/about", (req, res) => {
-  res.send("Hello from the About Page");
+  res.render("about", {
+    pageTitle: "About Page",
+    currentYear: new Date().getFullYear()
+  });
 });
 
 
